c24f0892b702e8fd1a662f3ea7e1ea17a29578ea
Casey <cgdable@up.edu.ph>
2021-03-31 21:58:16 +0800

    added onClick functionality to modal and exit button

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  
+  showSignUp: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onClickSignUp = () => {
+    this.showSignUp = !this.showSignUp;
+  }
+  onClickExit = () => {
+    this.onClickSignUp();
+  }
 }

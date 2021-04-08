fa497b6e8142b622527cce270365d8ab619e740b
Josiah Ocampo <cmocampo4@up.edu.ph>
2021-04-08 13:27:31 +0800

    Created TS Component file

---
+++ src/app/components/myaccountbuyer/myaccountbuyer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myaccountbuyer',
+  templateUrl: './myaccount.component.html',
+  styleUrls: ['./myaccount.component.css']
+})
+export class MyAccountBuyerComponent implements OnInit {
+ 
+  ngOnInit(): void {
+  }
+  
+}

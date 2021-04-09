5ebe6793acc11bf11f4ae14c8204d890ac5f196f
Josiah Ocampo <cmocampo4@up.edu.ph>
2021-04-09 20:06:51 +0800

    Updated "app.module.ts"
    updated to Import location of "myaccountbuyer" and "myaccountbuyer" folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { MyaccountbuyerComponent } from './myaccountbuyer/myaccountbuyer.component';
-import { MyaccountsellerComponent } from './myaccountseller/myaccountseller.component';
+import { MyaccountbuyerComponent } from './components/myaccountbuyer/myaccountbuyer.component';
+import { MyaccountsellerComponent } from './components/myaccountseller/myaccountseller.component';
 
 @NgModule({
   declarations: [

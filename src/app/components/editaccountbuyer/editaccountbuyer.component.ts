6e86fed166c404b02158dc5bbfff6b8514dd7b73
Josiah Ocampo <cmocampo4@up.edu.ph>
2021-04-23 12:28:40 +0800

    Added Initial Functionality in EditAccountBuyer Modal
    Updated FormBuilder and FormGroups for EditAccountBuyer, Added few functionality to buttons of the modal

--- src/app/components/editaccountbuyer/editaccountbuyer.component.ts
+++ src/app/components/editaccountbuyer/editaccountbuyer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editaccountbuyer',
@@ -8,19 +9,43 @@ import { Component, Input, OnInit } from '@angular/core';
 export class EditaccountbuyerComponent implements OnInit {
   @Input() openEditAccountBuyerModal: boolean = false;
   submitted: boolean = false;
+  editAccountBuyerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.editAccountBuyerForm = this.formBuilder.group ({
+      profilePhoto: [Validators.required],
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      address: ['', Validators.required],
+    });
   }
-  
+  get formControls() { return this.editAccountBuyerForm.controls; }
+
+  onClickChangePhoto = () => {
+    //insert code here to open file explorer
+  }
+
   onClickSave = () => {
-    // inser something here to save changes
-    this.openEditAccountBuyerModal = false;
+    this.submitted = true;  
   }
 
   onClickExit = () => {
     this.openEditAccountBuyerModal = false;
+    this.submitted = false;
+    this.editAccountBuyerForm.reset();
+  }
+
+  editAccountBuyer = async () => {
+    console.log(this.editAccountBuyerForm.value);
+    this.submitted = true;
+    if(this.editAccountBuyerForm.invalid){
+      return;
+    }
+    //this.userService.login(this.loginForm.value);
   }
 
 }

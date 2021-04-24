b535113c8ba8b02af35f0e1ca512e54b2dde83ed
Josiah Ocampo <cmocampo4@up.edu.ph>
2021-04-24 09:17:09 +0800

    Added Functionality to Modal UI for EditAccountSeller
    Updated linked modal to respective functions from MyAccountSeller to EditAccountSeller

--- src/app/components/editaccountseller/editaccountseller.component.ts
+++ src/app/components/editaccountseller/editaccountseller.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editaccountseller',
@@ -8,19 +9,43 @@ import { Component, Input, OnInit } from '@angular/core';
 export class EditaccountsellerComponent implements OnInit {
   @Input() openEditAccountSellerModal: boolean = false;
   submitted: boolean = false;
+  editAccountSellerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.editAccountSellerForm = this.formBuilder.group ({
+      profilePhoto: [Validators.required],
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      sellerDescription: ['', Validators.required],
+    });
   }
-  
+  get formControls() { return this.editAccountSellerForm.controls; }
+
+  onClickChangePhoto = () => {
+    //insert code here to open file explorer
+  }
+
   onClickSave = () => {
-    // inser something here to save changes
-    this.openEditAccountSellerModal = false;
+    this.submitted = true;  
   }
 
   onClickExit = () => {
     this.openEditAccountSellerModal = false;
+    this.submitted = false;
+    this.editAccountSellerForm.reset();
+  }
+
+  editAccountSeller = async () => {
+    console.log(this.editAccountSellerForm.value);
+    this.submitted = true;
+    if(this.editAccountSellerForm.invalid){
+      return;
+    }
+    //this.userService.login(this.loginForm.value);
   }
 
 }

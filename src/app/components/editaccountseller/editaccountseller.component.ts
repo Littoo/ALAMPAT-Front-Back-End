25a5a2d7d09a7f4bc21589d4637bfd4b908f77ff
Littoo <80081180+Littoo@users.noreply.github.com>
2021-04-25 00:01:54 +0800

    Updated EdiSeller but cannot update the edited data yet

--- src/app/components/editaccountseller/editaccountseller.component.ts
+++ src/app/components/editaccountseller/editaccountseller.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import axios from 'axios';
+
+
+const localAPI = 'http://localhost:3000'
 
 @Component({
   selector: 'app-editaccountseller',
@@ -11,9 +15,26 @@ export class EditaccountsellerComponent implements OnInit {
   submitted: boolean = false;
   editAccountSellerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef) { }
+  user:  any;
+  public imageSRC: string = '';
+  userID: string = '607fe491958fa65f08f14d0e';
+
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef,) { }
 
   ngOnInit(): void {
+
+    //getting and displaying the data of the  logged in user by UserId
+    axios.get(`${localAPI}/users/profile/` + this.userID)
+    .then(resp => {
+        this.user = resp.data.userData
+        this.imageSRC = `data:${this.user.profileImage?.contentType};base64,${this.user.profileImage?.imageBase64}`
+        console.log("user data: "+ JSON.stringify(this.user));
+    })
+    .catch(err => {
+        // Handle Error Here
+        console.error(err);
+    });
+
     this.editAccountSellerForm = this.formBuilder.group ({
       profileImage:[ null ],
       name: [''],
--- src/app/components/myaccountseller/myaccountseller.component.ts
+++ src/app/components/myaccountseller/myaccountseller.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios'
 import { AccountService } from 'src/app/services/user';
-import { Observable } from 'rxjs';
 import { User } from '../../models/User'
 import { DomSanitizer } from '@angular/platform-browser';
-import { Variable } from '@angular/compiler/src/render3/r3_ast';
+
 
 const localAPI = 'http://localhost:3000'
 
@@ -17,18 +16,18 @@ const localAPI = 'http://localhost:3000'
 export class MyaccountsellerComponent implements OnInit {
   showEditAccountSellerModal: boolean = false;
   user:  any;
-  image64 : any;
   public imageSRC: string = '';
   userID: string = '607fe491958fa65f08f14d0e';
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    //getting and displaying the data of the  logged in user by UserId
     axios.get(`${localAPI}/users/profile/` + this.userID)
     .then(resp => {
         this.user = resp.data.userData
         this.imageSRC = `data:${this.user.profileImage?.contentType};base64,${this.user.profileImage?.imageBase64}`
-        console.log("Hi"+JSON.stringify(this.user));
+        console.log("User data"+ JSON.stringify(this.user));
     })
     .catch(err => {
         // Handle Error Here
